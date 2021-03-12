d58b01da6259fbf4430ead65d1e6002fccaadb12
meenakshi-procloudware <meenakshi.procloudware@gmail.com>
2021-03-12 08:58:21 +0530

    status on march 11

--- heroes-app/src/app/app.component.spec.ts
+++ heroes-app/src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'heroes-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('heroes-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('heroes-app app is running!');
-  });
 });
--- heroes-app/src/app/heroes/heroes.component.spec.ts
+++ heroes-app/src/app/heroes/heroes.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroesComponent } from './heroes.component';
+import{ Hero } from '../hero';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
@@ -21,5 +22,16 @@ describe('HeroesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update selected hero', () => {
+    let h: Hero = {
+      id: 1,
+      name: 'hero1'
+    }
+    // act 
+    component.onSelect(h);
+    // expect 
+    expect(component.selectedHero).toBe(h);
   });
 });
